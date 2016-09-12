7e80914b1ba483cd84fd0bd3a82a050a2ff41aeb
Dan Bikle <bikle@bikle.com>
2016-09-11 23:01:00 -0700

    hello

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+  def compare
+  end
+
+  def whatif
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'pages/about'
+
+  get 'pages/blog'
+
+  get 'pages/contact'
+
+  get 'pages/compare'
+
+  get 'pages/whatif'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
