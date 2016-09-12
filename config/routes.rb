e6e45b7431480fbc2787dcb85f47189ba90c6947
Dan Bikle <bikle@bikle.com>
2016-09-11 23:36:58 -0700

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-
-  get 'pages/blog'
-
-  get 'pages/contact'
-
-  get 'pages/compare'
-
-  get 'pages/whatif'
-
+  get  'pages/about'
+  get  'pages/blog'
+  get  'pages/compare'
+  get  'pages/contact'
+  get  'pages/whatif'
+  root 'pages#about'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
