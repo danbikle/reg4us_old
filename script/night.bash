111c4662a486e0b99697f72409977a75687ae217
Dan Bikle <bikle@bikle.com>
2016-09-12 15:16:32 -0700

    hello

--- script/night.bash
+++ script/night.bash
@@ -2,7 +2,7 @@
 
 # ~/reg4us/script/night.bash
 
-# This script should generate some of the static content each night
+# This script should generate some static content each night
 # after the market has closed and the most recent GSPC-closing-price
 # is available from Yahoo.
 
@@ -14,5 +14,17 @@
 # I should create a folder to hold CSV data:
 mkdir -p ~/reg4us/public/csv/
 
-exit
+# I should get prices from Yahoo:
+/usr/bin/curl http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC > ~/reg4us/public/csv/gspc.csv
 
+# I should extract two columns and also sort:
+echo cdate,cp                                                              > ~/reg4us/public/csv/gspc2.csv
+sort ~/reg4us/public/csv/gspc.csv|awk -F, '{print $1"," $5}'|grep -v Date >> ~/reg4us/public/csv/gspc2.csv
+
+# I should compute features from the prices:
+~/anaconda3/bin/python genf.py SLOPES='[2,3,4,5,6,7,8,9]'
+
+# I should learn, test, and report:
+~/anaconda3/bin/python learn_tst_rpt.py TRAINSIZE=25 TESTYEAR=2016
+
+exit
