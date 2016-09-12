52f56be0c13b2442411b5e257c74dcdb59f221d9
Dan Bikle <bikle@bikle.com>
2016-09-12 15:05:53 -0700

    hello

---
+++ script/night.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# ~/reg4us/script/night.bash
+
+# This script should generate some of the static content each night
+# after the market has closed and the most recent GSPC-closing-price
+# is available from Yahoo.
+
+# The static content should help me compare effectiveness of GSPC
+# predictions computed from Linear Regression and Logistic Regression.
+
+# If you have questions, e-me: bikle101@gmail.com
+
+# I should create a folder to hold CSV data:
+mkdir -p ~/reg4us/public/csv/
+
+exit
+
